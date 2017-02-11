c1b920b0d86316785f8f2e87eddd9af72c8fced9
Gökçen Nurlu <gokcennurlu@gmail.com>
2017-02-11 10:00:02 +0100

    settings stuff

--- receipt_tracker/settings.py
+++ receipt_tracker/settings.py
@@ -85,10 +85,7 @@ WSGI_APPLICATION = 'receipt_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db()
 }
 
 
