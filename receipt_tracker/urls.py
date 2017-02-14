61c01eb1049bc8bfa3c829ecb7d01b8932cdc51b
teolemon <teolemon@users.noreply.github.com>
2017-02-14 17:48:05 +0100

    fix indent error

--- receipt_tracker/urls.py
+++ receipt_tracker/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     url(r'^', include('core.urls')),
 ]
 
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
-                                                                                        document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
