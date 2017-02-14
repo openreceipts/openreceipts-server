7fc2b799f707135049f7f97d645bcbf4d7c9baf5
teolemon <teolemon@users.noreply.github.com>
2017-02-14 16:48:06 +0100

    add stub for production.py

--- receipt_tracker/settings/production.py
+++ receipt_tracker/settings/production.py
@@ -1,1 +1,14 @@
+from __future__ import absolute_import
+import os
+import dj_database_url
 
+DATABASE_URL = ''
+from .base import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+    }
+}
+
+DATABASES['default'] =  dj_database_url.config()
