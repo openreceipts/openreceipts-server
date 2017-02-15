29ad6a6a02634d91b12499146a0b4e8ec1b1e6e7
teolemon <teolemon@users.noreply.github.com>
2017-02-15 11:19:29 +0100

    add whitenoise for basic static serving

--- receipt_tracker/wsgi_prod.py
+++ receipt_tracker/wsgi_prod.py
@@ -12,3 +12,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "receipt_tracker.settings.production")
 
 application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(application)
