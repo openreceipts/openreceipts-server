ee0ba6acbcb939e658294534f62dd500bf0a2699
Pierre Slamich <teolemon@users.noreply.github.com>
2017-11-13 15:24:57 +0100

    Update settings.py

--- receipt_tracker/settings.py
+++ receipt_tracker/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "https://s3.amazonaws.com/openreceiptsorg/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_files"),
