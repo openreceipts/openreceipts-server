09da3a9dae6e3fa56026e567d1955f93d054b170
Pierre Slamich <teolemon@users.noreply.github.com>
2017-11-13 15:31:31 +0100

    Update settings.py

--- receipt_tracker/settings.py
+++ receipt_tracker/settings.py
@@ -125,17 +125,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = ''
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = "https://s3.amazonaws.com/openreceiptsorg/"
+STATIC_ROOT = "https://s3.amazonaws.com/openreceiptsorg/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_files"),
 ]
 
-MEDIA_ROOT = "https://s3.amazonaws.com/openreceiptsorg/"
+MEDIA_ROOT = "https://s3.amazonaws.com/openreceiptsorg/media/"
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = ''
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
